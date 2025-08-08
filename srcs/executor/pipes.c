c98cf678629ab64dbaa61c269180b10e4d247adc
Mehmet Düşünen <mdusunen@k1m22s07.42istanbul.com.tr>
2025-08-08 19:28:51 +0300

    pipes dosyasında fazladan fonksiyonlar kaldı

--- srcs/executor/pipes.c
+++ srcs/executor/pipes.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipes.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yihakan <yihakan@student.42istanbul.com    +#+  +:+       +#+        */
+/*   By: mdusunen <mdusunen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 19:23:59 by yihakan           #+#    #+#             */
-/*   Updated: 2025/08/07 00:00:00 by yihakan          ###   ########.fr       */
+/*   Updated: 2025/08/08 19:15:03 by mdusunen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,15 +71,98 @@ static void	handle_child_process(t_command *cur, t_shell *shell,
 		execute_child_process(cur, shell, path);
 }
 
+static int	handle_empty_command_case(t_command *cur, t_pipe_data *data)
+{
+	if (data->has_heredoc_flag)
+		handle_heredoc_and_empty_cmd(cur, data->heredoc_pipe_fd,
+			data->pipe_fd, &data->prev_pipe_read);
+	else
+		handle_empty_command(cur, data->pipe_fd, &data->prev_pipe_read);
+	return (1);
+}
+
+static int	handle_builtin_single_case(t_command *cur, t_shell *shell,
+		char *path, t_pipe_data *data)
+{
+	if (is_builtin(cur->args[0]) && !cur->next
+		&& data->prev_pipe_read == -1 && !data->has_heredoc_flag)
+	{
+		data->last_status = handle_builtin_single_command(cur, shell, path);
+		data->last_cmd_not_found = 0;
+		return (1);
+	}
+	return (0);
+}
+
+static int	handle_command_not_found_case(t_command *cur, t_shell *shell,
+		char *path, t_pipe_data *data)
+{
+	if (!path && !is_builtin(cur->args[0]))
+	{
+		if (data->has_heredoc_flag)
+		{
+			data->last_status = handle_cmd_not_found_and_heredoc(cur,
+					shell, data->heredoc_pipe_fd, data->pipe_fd);
+			data->last_cmd_not_found = 1;
+		}
+		else
+		{
+			data->last_status = handle_command_not_found(cur, shell,
+					data->pipe_fd, data->prev_pipe_read);
+			if (cur->next)
+				data->last_cmd_not_found = 0;
+			else
+				data->last_cmd_not_found = 1;
+		}
+		return (1);
+	}
+	return (0);
+}
+
+static void	cleanup_after_fork(t_command *cur, t_pipe_data *data, char *path)
+{
+	if (data->has_heredoc_flag)
+		close_heredoc_fds(data->heredoc_pipe_fd);
+	if (data->prev_pipe_read != -1)
+		close(data->prev_pipe_read);
+	if (cur->next)
+	{
+		close(data->pipe_fd[1]);
+		data->prev_pipe_read = data->pipe_fd[0];
+	}
+	if (path)
+		free(path);
+}
+
+static int	process_single_command(t_command *cur, t_shell *shell,
+		t_pipe_data *data)
+{
+	char	*path;
+	pid_t	pid;
+
+	path = NULL;
+	if (!cur->args || !cur->args[0])
+		return (handle_empty_command_case(cur, data));
+	if (handle_builtin_single_case(cur, shell, path, data))
+		return (1);
+	if (!is_builtin(cur->args[0]))
+		path = find_executable(cur->args[0], shell);
+	if (handle_command_not_found_case(cur, shell, path, data))
+		return (1);
+	data->last_cmd_not_found = 0;
+	pid = fork();
+	if (pid == 0)
+		handle_child_process(cur, shell, path, data);
+	cleanup_after_fork(cur, data, path);
+	return (1);
+}
+
 int	execute_pipeline(t_command *cmds, t_shell *shell)
 {
 	t_command		*cur;
 	t_pipe_data		data;
-	pid_t			pid;
-	char			*path;
 
 	cur = cmds;
-	path = NULL;
 	data.prev_pipe_read = -1;
 	data.last_status = 0;
 	data.last_cmd_not_found = 0;
@@ -94,61 +177,10 @@ int	execute_pipeline(t_command *cmds, t_shell *shell)
 					data.pipe_fd, &data.prev_pipe_read))
 				return (1);
 		}
-		if (!cur->args || !cur->args[0])
-		{
-			if (data.has_heredoc_flag)
-				handle_heredoc_and_empty_cmd(cur, data.heredoc_pipe_fd,
-					data.pipe_fd, &data.prev_pipe_read);
-			else
-				handle_empty_command(cur, data.pipe_fd,
-					&data.prev_pipe_read);
+		if (process_single_command(cur, shell, &data))
 			cur = cur->next;
-			continue ;
-		}
-		if (is_builtin(cur->args[0]) && !cur->next
-			&& !data.prev_pipe_read && !data.has_heredoc_flag)
-		{
-			data.last_status = handle_builtin_single_command(cur,
-					shell, path);
-			data.last_cmd_not_found = 0;
-			cur = cur->next;
-			continue ;
-		}
-		if (!is_builtin(cur->args[0]))
-			path = find_executable(cur->args[0], shell);
-		if (!path && !is_builtin(cur->args[0]))
-		{
-			if (data.has_heredoc_flag)
-			{
-				data.last_status = handle_cmd_not_found_and_heredoc(cur,
-						shell, data.heredoc_pipe_fd, data.pipe_fd);
-				data.last_cmd_not_found = 1;
-			}
-			else
-			{
-				data.last_status = handle_command_not_found(cur, shell,
-						data.pipe_fd, data.prev_pipe_read);
-				data.last_cmd_not_found = cur->next ? 0 : 1;
-			}
-			cur = cur->next;
-			continue ;
-		}
-		data.last_cmd_not_found = 0;
-		pid = fork();
-		if (pid == 0)
-			handle_child_process(cur, shell, path, &data);
-		if (data.has_heredoc_flag)
-			close_heredoc_fds(data.heredoc_pipe_fd);
-		if (data.prev_pipe_read != -1)
-			close(data.prev_pipe_read);
-		if (cur->next)
-		{
-			close(data.pipe_fd[1]);
-			data.prev_pipe_read = data.pipe_fd[0];
-		}
-		if (path)
-			free(path);
-		cur = cur->next;
+		else
+			break ;
 	}
 	wait_for_children(&data.last_status, &data.last_cmd_not_found, shell);
 	return (data.last_status);
